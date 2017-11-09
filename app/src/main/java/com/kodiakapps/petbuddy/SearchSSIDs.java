8c57a38da4f44e1c724c1264b670f676a26e7be6
Cabana <usman.na.khan@gmail.com>
2017-11-08 19:00:51 -0800

    remove unnecessary wifi info fields

--- app/src/main/java/com/kodiakapps/petbuddy/SearchSSIDs.java
+++ app/src/main/java/com/kodiakapps/petbuddy/SearchSSIDs.java
@@ -21,40 +21,32 @@ import java.util.List;
  * Created by usman on 5/29/17.
  */
 
-public class SearchSSIDs extends Activity implements View.OnClickListener
-{
+public class SearchSSIDs extends Activity {
     private static final String TAG = "PetBuddy";
     private static final String DEVICE_SERIAL = "serialno";
     private String deviceName = "";
     private String serialno;
-    private String homeWifiSSID = "";
-    private String homeWifiPSK = "";
 
     WifiManager wifi;
     TextView textStatus;
     TextView connectNetworkTV;
-    Button buttonScan;
-    Button buttonWifiInfoSubmit;
-    EditText homeSsidEt;
-    EditText homePskEt;
     List<ScanResult> results;
 
     /* Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_ssid);
 
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
-            if(extras == null) {
-                serialno= null;
+            if (extras == null) {
+                serialno = null;
             } else {
-                serialno= extras.getString(DEVICE_SERIAL);
+                serialno = extras.getString(DEVICE_SERIAL);
             }
         } else {
-            serialno= (String) savedInstanceState.getSerializable(DEVICE_SERIAL);
+            serialno = (String) savedInstanceState.getSerializable(DEVICE_SERIAL);
         }
 
         deviceName = "PB" + serialno;
@@ -63,29 +55,21 @@ public class SearchSSIDs extends Activity implements View.OnClickListener
         Log.d(TAG, "PSK is: " + serialno);
 
         textStatus = (TextView) findViewById(R.id.textStatus);
-        textStatus.setVisibility(View.INVISIBLE);
+        textStatus.setVisibility(View.VISIBLE);
         connectNetworkTV = (TextView) findViewById(R.id.connectNetworkTV);
-        connectNetworkTV.setVisibility(View.INVISIBLE);
-        buttonScan = (Button) findViewById(R.id.buttonScan);
-        buttonScan.setOnClickListener(this);
-        buttonScan.setVisibility(View.INVISIBLE);
+        connectNetworkTV.setVisibility(View.VISIBLE);
 
-        buttonWifiInfoSubmit = (Button) findViewById(R.id.submitWifiInfoButton);
-        buttonWifiInfoSubmit.setOnClickListener(this);
+        Log.d(TAG, "buttons and textstatuses done");
 
-        homeSsidEt = (EditText) findViewById(R.id.enterSsid);
-        homePskEt = (EditText) findViewById(R.id.enterPsk);
-
-        final String serialno;
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
-            if(extras == null) {
-                serialno= null;
+            if (extras == null) {
+                serialno = null;
             } else {
-                serialno= extras.getString(DEVICE_SERIAL);
+                serialno = extras.getString(DEVICE_SERIAL);
             }
         } else {
-            serialno= (String) savedInstanceState.getSerializable(DEVICE_SERIAL);
+            serialno = (String) savedInstanceState.getSerializable(DEVICE_SERIAL);
         }
 
         deviceName = "PB" + serialno;
@@ -94,21 +78,25 @@ public class SearchSSIDs extends Activity implements View.OnClickListener
         Log.d(TAG, "PSK is: " + serialno);
 
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        if (wifi.isWifiEnabled() == false)
-        {
+        if (wifi.isWifiEnabled() == false) {
             Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
             wifi.setWifiEnabled(true);
         }
 
-        registerReceiver(new BroadcastReceiver()
-        {
+        Log.d(TAG, "Starting WiFi scan");
+        wifi.startScan();
+
+        Toast.makeText(this, "Scanning...", Toast.LENGTH_SHORT).show();
+        textStatus.setText("Scanning");
+
+        registerReceiver(new BroadcastReceiver() {
             @Override
-            public void onReceive(Context c, Intent intent)
-            {
+            public void onReceive(Context c, Intent intent) {
+                Log.d(TAG, "onReceive()");
                 Toast.makeText(getApplicationContext(), "got Scan Results", Toast.LENGTH_LONG).show();
                 textStatus.setText("Receiving");
                 results = wifi.getScanResults();
-                if(results.toString().contains(deviceName)) {
+                if (results.toString().contains(deviceName)) {
                     Toast.makeText(getApplicationContext(), "PetBuddy Found!", Toast.LENGTH_LONG).show();
 
                     WifiConfiguration myWiFiConfig = new WifiConfiguration();
@@ -122,48 +110,14 @@ public class SearchSSIDs extends Activity implements View.OnClickListener
                     myWiFiConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
                     myWiFiConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
                     int res = wifi.addNetwork(myWiFiConfig);
-                    Log.d(TAG, "addNetwork returned " + res );
-                    boolean  networkEnabled = wifi.enableNetwork(res, true);
-                    Log.d(TAG, "enableNetwork returned " + networkEnabled );
-                }
-                else {
+                    Log.d(TAG, "addNetwork returned " + res);
+                    boolean networkEnabled = wifi.enableNetwork(res, true);
+                    Log.d(TAG, "enableNetwork returned " + networkEnabled);
+                } else {
                     Toast.makeText(getApplicationContext(), "Could not find your PetBuddy :(", Toast.LENGTH_LONG).show();
                 }
 
             }
         }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-    }
-
-    public void onClick(View view)
-    {
-        switch(view.getId())
-        {
-            case R.id.buttonScan:
-                Log.d(TAG, "Starting WiFi scan");
-                wifi.startScan();
-
-                Toast.makeText(this, "Scanning...", Toast.LENGTH_SHORT).show();
-                textStatus.setText("Scanning");
-                break;
-
-            case R.id.submitWifiInfoButton:
-
-
-                homeWifiSSID = homeSsidEt.getText().toString();
-                homeWifiPSK = homePskEt.getText().toString();
-
-                homeSsidEt.setVisibility(View.GONE);
-                homePskEt.setVisibility(View.GONE);
-                buttonWifiInfoSubmit.setVisibility(View.GONE);
-                TextView enterWifiInfo = (TextView) findViewById(R.id.enterWifiInfo);
-                enterWifiInfo.setVisibility(View.GONE);
-
-                textStatus.setVisibility(View.VISIBLE);
-                connectNetworkTV.setVisibility(View.VISIBLE);
-                buttonScan.setVisibility(View.VISIBLE);
-                Log.d(TAG, "User input ssid=" + homeWifiSSID + "  psk=" + homeWifiPSK);
-                break;
-        }
-
     }
 }
