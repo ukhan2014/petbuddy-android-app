f133c6d111042a05fb8cd300c19ecf56ab466801
Cabana <usman.na.khan@gmail.com>
2017-11-11 22:26:33 -0800

    add new starting activity

---
+++ app/src/main/java/com/kodiakapps/petbuddy/StartActivity.kt
@@ -0,0 +1,15 @@
+package com.kodiakapps.petbuddy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    private val count = 0
+    private val startMillis: Long = 0
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
