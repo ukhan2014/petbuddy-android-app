e63edd78202ef6755ec470ba57e1300a9b222ef5
Cabana <usman.na.khan@gmail.com>
2017-11-12 23:20:49 -0800

    try to sort wifi scan list ascending

--- app/src/main/java/com/kodiakapps/petbuddy/WifiScanActivity.java
+++ app/src/main/java/com/kodiakapps/petbuddy/WifiScanActivity.java
@@ -10,6 +10,7 @@ import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -17,6 +18,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
@@ -44,6 +47,13 @@ public class WifiScanActivity extends Activity implements View.OnClickListener
         buttonScan = (Button) findViewById(R.id.buttonScan);
         buttonScan.setOnClickListener(this);
         lv = (ListView)findViewById(R.id.list);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String) lv.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(),"You selected : " + item,Toast.LENGTH_SHORT).show();
+            }
+        });
 
         wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         if (wifi.isWifiEnabled() == false)
@@ -80,11 +90,29 @@ public class WifiScanActivity extends Activity implements View.OnClickListener
             while (size >= 0)
             {
                 HashMap<String, String> item = new HashMap<String, String>();
-                item.put(ITEM_KEY, results.get(size).SSID + "  " + results.get(size).capabilities);
+                item.put(ITEM_KEY, results.get(size).level + "," + results.get(size).SSID + ","
+                        + results.get(size).capabilities);
 
                 arraylist.add(item);
+                Collections.reverse(arraylist);
+//                Collections.sort(arraylist, new Comparator<String>() {
+//                    public int compare(String s1, String s2) {
+//                        int secondCommaIdx_s1 = s1.indexOf(',', 1 + s1.indexOf(','));
+//                        int secondCommaIdx_s2 = s2.indexOf(',', 1 + s2.indexOf(','));
+//
+//                        int signalStrength_s1 = Integer.parseInt(s1.substring(secondCommaIdx_s1+1));
+//                        int signalStrength_s2 = Integer.parseInt(s2.substring(secondCommaIdx_s2+1));
+//
+//                        if (signalStrength_s1 >= signalStrength_s2) {
+//                            return -1;
+//                        } else {
+//                            return 1;
+//                        }
+//                    }
+//                });
                 size--;
                 adapter.notifyDataSetChanged();
+
             }
         }
         catch (Exception e)
