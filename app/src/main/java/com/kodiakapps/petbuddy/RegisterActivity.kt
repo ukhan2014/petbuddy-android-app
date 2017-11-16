57d6ff1d470ca82640d4a13701e7013d0c646269
Cabana <usman.na.khan@gmail.com>
2017-11-15 19:04:51 -0800

    grab registration info from user and send it to next activity

--- app/src/main/java/com/kodiakapps/petbuddy/RegisterActivity.kt
+++ app/src/main/java/com/kodiakapps/petbuddy/RegisterActivity.kt
@@ -1,12 +1,20 @@
 package com.kodiakapps.petbuddy
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.widget.TextView
+import org.json.JSONObject
+
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val TAG = "PBD:RegisterActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,21 +44,31 @@ class RegisterActivity : AppCompatActivity() {
                         "Choose a password", Toast.LENGTH_LONG).show()
 
                 else -> {
-                    et_fname.setText("")
-                    et_lname.setText("")
-                    et_email.setText("")
-                    et_pwd.setText("")
+                    et_fname.visibility = View.INVISIBLE
+                    et_lname.visibility = View.INVISIBLE
+                    et_email.visibility = View.INVISIBLE
+                    et_pwd.visibility = View.INVISIBLE
 
-                    println("firstname = " + firstname)
-                    println("lastname = " + lastname)
-                    println("email = " + emailadd)
-                    println("pwassword = " + pwd)
+                    Log.d(TAG, "User input:")
+                    Log.d(TAG, "firstname = " + firstname)
+                    Log.d(TAG, "lastname = " + lastname)
+                    Log.d(TAG, "email = " + emailadd)
+                    Log.d(TAG, "password = " + pwd)
 
-                    Toast.makeText(this@RegisterActivity, "logged in as: "+ firstname, Toast.LENGTH_LONG).show()
+                    val RegJSON = JSONObject()
+                    RegJSON.put("fname", firstname)
+                    RegJSON.put("lname", lastname)
+                    RegJSON.put("email", emailadd)
+                    RegJSON.put("pwd", pwd)
+
+                    val myIntent = Intent(this@RegisterActivity,
+                            SendInfoToPbd::class.java)
+                    myIntent.putExtra("reginfo", RegJSON.toString())
+                    startActivity(myIntent)
+
+                    finish()
                 }
             }
         }
     }
-
-
 }
