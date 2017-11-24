980e21fe60fed9f076098efd716345a6b3405b4b
Cabana <usman.na.khan@gmail.com>
2017-11-24 02:34:36 -0800

    display user selection to usr and prompt to confirm

--- app/src/main/java/com/kodiakapps/petbuddy/ConfirmWiFiSelectionActivity.java
+++ app/src/main/java/com/kodiakapps/petbuddy/ConfirmWiFiSelectionActivity.java
@@ -1,13 +1,25 @@
 package com.kodiakapps.petbuddy;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ConfirmWiFiSelectionActivity extends AppCompatActivity {
+
+    private TextView selectedWifiTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_wi_fi_selection);
+
+        Resources res = getResources();
+
+        String ssid = (String)(getIntent().getExtras().get("ssid"));
+
+        selectedWifiTextView = (TextView) findViewById(R.id.selected_wifi_textview);
+        String text = String.format(res.getString(R.string.selectedWifi_str), ssid);
+        selectedWifiTextView.setText(text);
     }
 }
